1046b6a03d488d6515d1177efd30c76b7833a7a9
Cristina Onea <cristinaonea86@gmail.com>
2022-05-21 17:32:41 +0000

    Add quantity input buttons

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect 
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,15 +6,15 @@ def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
-  
+
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    size = None 
+    size = None
     if 'product_size' in request.POST:
-        size = request.POST ['product_size']
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
     if size:
@@ -33,3 +33,4 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+    
